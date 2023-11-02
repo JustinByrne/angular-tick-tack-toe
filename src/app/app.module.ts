7ce905c828d738302dd1aeedde38af09090a72a9
Justin <theskaterjesus@gmail.com>
2023-11-02 22:00:00 +0000

    Added angular material styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SquareComponent,
-    BoardComponent
+    BoardComponent,
   ],
   imports: [
+    SquareComponent,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-square',
   template: `
-    <button>
+    <button mat-flat-button [color]="value == null ? 'primary' : value == 'X' ? 'accent' : 'warn'">
       {{ value }}
     </button>
   `,
   styles: [
-    'button { margin: 0; height: 100%; width: 100%; font-size: 3rem; }',
-  ]
+    'button { margin: 0; height: 100% !important; width: 100%; font-size: 3rem; }',
+  ],
+  standalone: true,
+  imports: [MatButtonModule],
 })
 export class SquareComponent {
   @Input() value!: 'X' | 'O';
